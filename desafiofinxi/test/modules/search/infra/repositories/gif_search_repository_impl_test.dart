4ae91d31441d8c189bb5b659432fceb4d047b854
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2020-11-14 17:34:36 -0300

    definida camada de infra-estrutura e seus testes de unidade

--- desafiofinxi/test/modules/search/infra/repositories/gif_search_repository_impl_test.dart
+++ desafiofinxi/test/modules/search/infra/repositories/gif_search_repository_impl_test.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:desafiofinxi/modules/search/domain/entities/gif.dart';
+import 'package:desafiofinxi/modules/search/domain/errors/errors.dart';
 import 'package:desafiofinxi/modules/search/infra/datasources/gif_search_datasource.dart';
 import 'package:desafiofinxi/modules/search/infra/models/gif_model.dart';
 import 'package:desafiofinxi/modules/search/infra/repositories/gif_search_repository_impl.dart';
@@ -12,9 +13,21 @@ final datasource = SearchDatasourceMock();
 final repository = GifSearchRepository(datasource);
 
 main(){
-  test("Must return a Gif list", () async{
+  test("Deve retornar uma lista de gifs matadores!", () async{
     when(datasource.gifSearch(any)).thenAnswer((realInvocation) async => List<GifModel>());
     final result = await repository.searchGif("RodarthVader");
     expect(result | null, isA<List<Gif>>());
+  });
+
+  test("Deve retornar uma lista de gifs matadores!", () async{
+    when(datasource.gifSearch(any)).thenAnswer((realInvocation) async => List<GifModel>());
+    final result = await repository.searchGif("");
+    expect(result | null, isA<List<Gif>>());
+  });
+
+  test("Deve retornar uma exceção do tipo DataSourceError", () async{
+    when(datasource.gifSearch(any)).thenAnswer((realInvocation) async => List<GifModel>());
+    final result = await repository.searchGif(null);
+    expect(result.fold(id,id), isA<DataSourceError>());
   });
 }
