b8be434f0239c0c3dbc46ccee78a2ea216d9be6a
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2020-11-14 17:00:38 -0300

    definida camada de dominio e testes de unidade

---
+++ desafiofinxi/test/modules/search/domain/usecases/search_gif_by_text_test.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:desafiofinxi/modules/search/domain/entities/gif.dart';
+import 'package:desafiofinxi/modules/search/domain/errors/errors.dart';
+import 'package:desafiofinxi/modules/search/domain/repositories/gif_search_repository.dart';
+import 'package:desafiofinxi/modules/search/domain/usecases/search_gif_by_text.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class SearchRepositoryMock extends Mock implements IGifSearchRepository{}
+
+final repository = SearchRepositoryMock();
+final usecase = SearchGifByText(repository);
+
+main(){
+
+  test("Deve retornar uma lista de gif's matadores", () async {
+    when(repository.searchGif(any)).thenAnswer((realInvocation) async => Right(List<Gif>()));
+    final result = await usecase.searchGif("jarjar");
+    expect(result | null, isA<List<Gif>>());
+  });
+
+  test("Deve retornar uma lista de gif's matadores", () async {
+    when(repository.searchGif(any)).thenAnswer((realInvocation) async => Right(List<Gif>()));
+    final result = await usecase.searchGif("");
+    expect(result | null, isA<List<Gif>>());
+  });
+
+  test("Deve retornar uma exceção do tipo InvalidTextError", () async{
+    when(repository.searchGif(any)).thenAnswer((realInvocation) async => Right(List<Gif>()));
+    final result = await usecase.searchGif(null);
+    expect(result.fold(id,id), isA<InvalidTextError>());
+  });
+}
